964a758cf2773ff54d906869175fbd9bdfbdd530
hsrsbd <hsraffety@gmail.com>
2019-06-14 08:29:14 -0700

    building out app.js, routes, functions. smurf is a weird word.

--- village/src/App.js
+++ village/src/App.js
@@ -1,11 +1,13 @@
 // dependencies
 import React, { Component } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 // styles
 import './App.css';
 
 // components
+import Home from './components/Home';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
@@ -29,14 +31,51 @@ class App extends Component {
       });
   }
 
+  addSmurf = smurf => {
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+        this.setState({ smurfs: res.data });
+        this.props.history.push('/smurfs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className='App'>
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <div className='nav'>
+          <NavLink exact to='/' className='navlink'>
+            <h1>Smurfemon - don't catch them all.</h1>
+          </NavLink>
+          <span class='sub'>All The Smurfs!</span>
+          <nav>
+            <NavLink exact to='/' className='navlink'>
+              Home
+            </NavLink>
+            <NavLink to='/smurfs' className='navlink'>
+              Smurfs
+            </NavLink>
+            <NavLink to='/smurf-form' className='navlink'>
+              Add A Smurf
+            </NavLink>
+          </nav>
+        </div>
+        <Route exact path='/' component={Home} />
+        <Route
+          exact
+          path='/smurf-form'
+          render={props => <SmurfForm {...props} addSmurf={this.addSmurf} />}
+        />
+        <Route
+          path='/smurfs'
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
---
+++ village/src/components/Home.js
@@ -0,0 +1,13 @@
+// dependencies
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <h1>Hi from home</h1>
+    </div>
+  );
+};
+
+export default Home;
